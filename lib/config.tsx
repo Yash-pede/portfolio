0a4b165ec866b4c3f349508a11005e019260a17a
yash-pede <yashpede@ssipmt.com>
2024-08-03 00:26:59 +0530

    feat: hero title

--- lib/config.tsx
+++ lib/config.tsx
@@ -38,3 +38,20 @@ export const menuItems = [
   "Help & Feedback",
   "Log Out",
 ];
+
+export const HeroTitleData = [
+  {
+    route: "/",
+    title: [
+      `Hey, I'm ${SiteConfig.user.firstName}!`,
+      "I welcome to my corner of the internet!",
+    ],
+  },
+  {
+    route: "/about",
+    title: [
+      `Good ${new Date().getHours() > 12 ? "morning" : "evening"}!`,
+      `I'm ${SiteConfig.user.firstName}, an experienced frontend developer.`,
+    ],
+  },
+];
